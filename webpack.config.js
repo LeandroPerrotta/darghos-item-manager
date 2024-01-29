84b0210413b1a51cfd73585061fd3ff69cd939db
Leandro Perrotta <leandro_perrotta@hotmail.com>
2024-01-29 19:25:53 -0300

    chore(project): adding base files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // ou 'production'
+    entry: './src/ui-core/views/index.js', // Ponto de entrada da sua aplicação
+    output: {
+        path: path.resolve(__dirname, 'app/electron'), // Onde os arquivos empacotados serão colocados
+        filename: 'bundle.js' // O nome do arquivo empacotado
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Transpilar todos os arquivos .js
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'] // Presets do Babel que você está usando
+                    }
+                }
+            },{
+                test: /\.css$/, // Regra para arquivos .css
+                use: ['style-loader', 'css-loader'] // Utiliza style-loader e css-loader
+            }
+        ]
+    },
+    target: 'electron-renderer'
+};
